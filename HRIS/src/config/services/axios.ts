9237932576f762a9c88b481e9bfc77ce45f90a4f
Aulkanic <trazonamichael.pdm@gmail.com>
2024-05-05 22:03:31 +0800

    das

--- HRIS/src/config/services/axios.ts
+++ HRIS/src/config/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1/',
+  baseURL: 'https://hris-api.savant-sage.com/api/v1/',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
     'Content-Type': 'multipart/form-data',
